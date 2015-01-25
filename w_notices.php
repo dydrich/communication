850267bf910e06970d07513485876da5fae615dc
dydrich <dydrich@yahoo.com>
2015-01-25 20:48:25 +0100

    eliminato in w_notices il riferimento ai documenti in evidenza

--- w_notices.php
+++ w_notices.php
@@ -1,12 +1,9 @@
 <?php
 
-$sel_ev_docs = "SELECT COUNT(id) FROM rb_documents WHERE evidenziato IS NOT NULL AND evidenziato > NOW()";
-$res_ev = $db->executeCount($sel_ev_docs);
-
 $sel_notices = "SELECT * FROM rb_com_avvisi WHERE data_scadenza >= NOW()";
 $res_notices = $db->executeQuery($sel_notices);
 
-if ($res_ev > 0 || $res_notices->num_rows > 0){
+if ($res_notices->num_rows > 0){
 ?>
 	<div class="welcome">
 		<p id="w_head">Avviso <?php echo date("d/m/Y") ?></p>
@@ -19,9 +16,6 @@ if ($res_ev > 0 || $res_notices->num_rows > 0){
 			}
 		}
 		?>
-		<?php if ($res_ev > 0): ?>
-		<p class="w_text attention" style="">&middot; Sono presenti dei documenti segnalati come importanti &middot;</p>
-		<?php endif; ?>
 	</div>
 <?php 
 }
