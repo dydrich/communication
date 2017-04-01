06eebe98ce5d0733c9bb25ee9ba864b47216fcce
Riccardo Bachis <dydrich@yahoo.com>
2017-04-01 20:53:36 +0200

    verifica per mostrare solo eventi anno in corso

--- events.php
+++ events.php
@@ -8,7 +8,11 @@ check_permission(DIR_PERM|DSG_PERM|SEG_PERM|APS_PERM|AIS_PERM|AMS_PERM);
 $limit = 10;
 $offset = 0;
 
-$sel_evs = "SELECT id_evento, abstract, testo, data_evento, data_inserimento, data_modifica, rb_utenti.nome AS nome, rb_utenti.cognome AS cg, pubblico, modificabile FROM rb_com_eventi, rb_utenti WHERE rb_com_eventi.owner = rb_utenti.uid ORDER BY data_evento DESC, id_evento DESC ";
+$sel_evs = "SELECT id_evento, abstract, testo, data_evento, data_inserimento, data_modifica, rb_utenti.nome AS nome, rb_utenti.cognome AS cg, pubblico, modificabile 
+FROM rb_com_eventi, rb_utenti 
+WHERE rb_com_eventi.owner = rb_utenti.uid 
+AND data_evento >= '{$_SESSION['__current_year__']->get_data_apertura()}' 
+ORDER BY data_evento DESC, id_evento DESC ";
 
 $res_evs = $db->execute($sel_evs);
 //print $sel_links;
