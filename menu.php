13724d9c4c43cc36b6cfba3a014d8152c9262035
Riccardo Bachis <dydrich@yahoo.com>
2018-09-12 10:40:52 -0400

    lettura circolari per dsga

--- menu.php
+++ menu.php
@@ -22,7 +22,7 @@
 	if($_SESSION['__user__']->check_perms(DIR_PERM|DSG_PERM|SEG_PERM|DOC_PERM|ATA_PERM) == true): ?>
 	<p class="menu_label data_icon">Circolari</p>
 	<ul class="menublock" style="" dir="rtl">
-	<?php if($_SESSION['__user__']->check_perms(DOC_PERM|ATA_PERM|SEG_PERM) == true): ?>
+	<?php if($_SESSION['__user__']->check_perms(DOC_PERM|ATA_PERM|SEG_PERM|DSG_PERM) == true): ?>
 		<li><a href="vedi_circolari.php" style="text-decoration: none">Leggi circolari</a></li>
 	<?php endif; ?>
 	<?php if($_SESSION['__user__']->check_perms(DIR_PERM|DSG_PERM|SEG_PERM) == true): ?>
